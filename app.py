decf086a44a8b917cf81beaa69735e7ca4f83df0
AlexPyvovarov <olekiipyvovarov@gmail.com>
2024-10-19 13:15:13 -0300

    bug fix

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request
-from database import Posts, SQLModel, session
+from flask import Flask, render_template, request, redirect, url_for
+from database import Posts, session
 from sqlmodel import select
+from dataclasses import dataclass
 
 
 app = Flask(__name__)
-
+app.secret_key = "iuub#$hnn8h90DSfh;"
 
 
 @app.get("/")
@@ -15,7 +16,7 @@ def index():
 @app.get("/posts")
 def get_posts():
     posts = session.scalars(select(Posts)).all()
-    return render_template("index.html", posts=posts)
+    return render_template("index.html", posts = posts)
 
 
 @app.get("/post_view")
@@ -23,22 +24,34 @@ def post_view():
     id = int(request.args.get("post_id"))
     post = session.get_one(Posts, id)
     post = post.model_dump()
-    return render_template("view.html", post=post)
+    return render_template("view.html", post = post)
+
+
+@dataclass
+class PostForm:
+    title: str
+    content: str
+
+
+@app.get("/posts/create")
+def post_form():
+    return render_template("form.html", post = PostForm("", "").__dict__)
 
 
 @app.post("/posts/create")
 def post_create():
-    pass
-
-
-def post_mock_data():
-    posts = [Posts(title=f"{x}", content=f"{x}") for x in range(1, 11)]
-    session.add_all(posts)
-    session.commit()
-
-
+    form_data = request.form
+    post = Posts(
+        content = form_data.get("content"),
+        title = form_data.get("title"),
+    )
+    if post.title and post.content:
+        session.add(post)
+        session.commit()
+        print(f"{form_data=}")
+        return redirect(url_for(index.__name__))
+    return render_template("form.html", post = PostForm("", "").__dict__)
 
 
 if __name__ == "__main__":
-    post_mock_data()
     app.run(debug=True)
